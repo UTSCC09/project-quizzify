e64a34a074ac2364020067c8f750d55047d301ac
Henry <36207470+henryinqz@users.noreply.github.com>
2023-11-10 11:52:40 -0500

    Frontend Auth0

--- quizzify/frontend/pages/_app.js
+++ quizzify/frontend/pages/_app.js
@@ -1,19 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../lib/theme'
-import Head from 'next/head'
-import '@fontsource-variable/manrope';
 
+import { Auth0Provider } from '@auth0/auth0-react';
+import { usePathname } from 'next/navigation'
+
+import '@fontsource-variable/manrope';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-    return (
-        <div>
-        <Head>
-            <title>Quizzify</title>
-        </Head>
-        <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
-        </ChakraProvider>
-        </div>
-    )
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: process.env.NEXT_PUBLIC_FRONTEND_BASE_URL + usePathname()
+      }}
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Auth0Provider>
+  );
 }
--- quizzify/frontend/pages/index.js
+++ quizzify/frontend/pages/index.js
@@ -1,9 +1,24 @@
+import LoginButton from "@/components/Buttons/Auth/LoginButton";
+import LogoutButton from "@/components/Buttons/Auth/LogoutButton";
 import { Button } from "@chakra-ui/react";
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Home() {
-    return (
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    getAccessTokenSilently,
+  } = useAuth0();
+  return (
+    <>
+      <Button>Quizzify</Button>
+      {!isAuthenticated ? <LoginButton/> :
         <>
-            <Button>Quizzify</Button>
+          <LogoutButton/>
+          <h1>Signed in as {user.name}</h1>
         </>
-    )
+      }
+    </>
+  )
 }
