c23cb9e4d5de027df4d978fca6490e4073ebed27
Henry <36207470+henryinqz@users.noreply.github.com>
2023-11-10 22:08:26 -0500

    resitrct quiz question types

--- quizzify/backend/models/quiz.js
+++ quizzify/backend/models/quiz.js
@@ -1,23 +1,39 @@
 const mongoose = require("mongoose")
 
+const SINGLE_CHOICE = "SINGLE_CHOICE"
+const MULTIPLE_CHOICE = "MULTIPLE_CHOICE"
+const TRUE_OR_FALSE= "TRUE_OR_FALSE"
+const FILL_BLANK = "FILL_BLANK"
+
+QUIZ_TYPES = [SINGLE_CHOICE, MULTIPLE_CHOICE, TRUE_OR_FALSE, FILL_BLANK]
+
 const QuizSchema = new mongoose.Schema({
   userId: {type: String, required: true, index: true},
   name: {type: String, required: true, index: true},
-  private: {type: Boolean, required: true},
+  private: {
+    type: Boolean, 
+    required: true, 
+    default: false
+  },
   questions: [{
     question: {type: String, required: true},
-    type: {type: String, required: true}, // idk if we do indices or string
+    type: {
+      type: String, 
+      required: true, 
+      enum: QUIZ_TYPES,
+      default: SINGLE_CHOICE
+    },
     responses: [{
       response: {type: String, required: true},
       isAnswer: {type: Boolean, required: true}
     }],
-    img: {
-      data: Buffer,
-      contentType: String
-    }
   }],
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true
+  },
 })
-
 
 QuizSchema.statics = {
   create: function(userId, name, private, questions) {
