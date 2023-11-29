9b80ce6a043a01f04c24be7860c4ff0f66bd10ca
Peter Chow <peterp.chow@mail.utoronto.ca>
2023-11-28 20:36:03 -0500

    removed line

--- quizzify/frontend/components/Game/PlayerPlay.js
+++ quizzify/frontend/components/Game/PlayerPlay.js
@@ -46,7 +46,6 @@ export default function PlayerPlay({
     }
 
     const handleSubmit = () => {
-        setSubmitted2(true);
         socket.emit(SOCKET_EVENTS.PLAYER.answer, {
             joinCode: gameCode.toLowerCase(), 
             selectedAnswers: selectedAnswers
